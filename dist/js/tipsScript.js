0aa55819f90da19529a6d0fc90876259f15267e1
Benjie silvano <103920174+kratosman@users.noreply.github.com>
2023-06-04 21:10:34 +0800

    Update tipsScript.js

--- dist/js/tipsScript.js
+++ dist/js/tipsScript.js
@@ -1,8 +1,8 @@
 var tipsTime = {
-    morning_coffee: 'Enjoy a refreshing cup of coffee to fuel your morning.',
-    noon_coffee: 'Take a moment to relax and savor another delicious cup of coffee.',
-    afternoon: "It's time for a well-deserved coffee break to boost your energy.",
-    afternoon_night: 'Wind down your day with a soothing cup of coffee.'
+    morning_coffee: 'Kickstart your day with a delightful brew of coffee to energize your morning.',
+    noon_coffee: 'Indulge in a heavenly cup of coffee and experience a moment of pure bliss.',
+    afternoon: "Treat yourself to a rejuvenating coffee break and embrace a burst of productivity.",
+    afternoon_night: 'Unwind and relax with a tantalizing cup of coffee, a perfect companion for a serene evening.'
 }
 
 var remindersTime;
@@ -30,10 +30,10 @@ function createReminderFunc(time, message) {
 }
 
 function checkReminders() {
-    const date = new Date();
-    const dateHours = date.getHours();
-    const dateMinutes = date.getMinutes();
-    const period = dateHours >= 12 ? 'PM' : 'AM';
+    var date = new Date();
+    var dateHours = date.getHours();
+    var dateMinutes = date.getMinutes();
+    var period = dateHours >= 12 ? 'PM' : 'AM';
     
     
     const {morning_coffee, noon_coffee, afternoon, afternoon_night} = tipsTime;
@@ -50,7 +50,7 @@ function checkReminders() {
     } else if (dateHours === 3 && dateMinutes === 00 && period === 'PM') {
         createReminderFunc('3:00 AM', afternoon);
     } else if (dateHours === 6 && dateMinutes === 00 && period === 'PM'){
-        createReminderFunc('3:00 AM', afternoon_night);
+        createReminderFunc('6:00 AM', afternoon_night);
     } else if (dateMinutes >= 50) {
         clearInterval(remindersTime);
     }

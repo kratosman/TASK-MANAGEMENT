a77012cc0e70decab625db8846fb236c91a98cce
Benjie silvano <103920174+kratosman@users.noreply.github.com>
2023-06-05 07:00:17 +0800

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -297,72 +297,76 @@ boards.forEach((board, currentIndex) => {
                 </div>`;
                 
                     for (let ins = 0; ins < sectionItem.children.length; ins++) {
-                        sectionItem.children[ins].addEventListener('click', () => {
-                            console.log(sectionItem.children[ins]);
-                            var dataList = boardsTaskStorage[currentIndex].task_log[ins].subtask.map(itemList => itemList.statusList);
-                    var taskDetailModal = document.createElement('div');
-                        taskDetailModal.classList.add('task-detail-modal');
-                        
-                        taskDetailModal.innerHTML = '';
-                        
-                        taskDetailModal.innerHTML += `<!-- TASK CONTENT -->
-                            <div class="task-detail-modal-contents">
-                            <div class="task-detail-title">
-                                <span>
-                                <h1>${boardsTaskStorage[currentIndex].task_log[ins].task_name}</h1>
-                                </span>
-                                <span>
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 6C10.5 5.60218 10.658 5.22064 10.9393 4.93934C11.2206 4.65804 11.6022 4.5 12 4.5C12.3978 4.5 12.7794 4.65804 13.0607 4.93934C13.342 5.22064 13.5 5.60218 13.5 6C13.5 6.39782 13.342 6.77936 13.0607 7.06066C12.7794 7.34196 12.3978 7.5 12 7.5C11.6022 7.5 11.2206 7.34196 10.9393 7.06066C10.658 6.77936 10.5 6.39782 10.5 6ZM10.5 12C10.5 11.6022 10.658 11.2206 10.9393 10.9393C11.2206 10.658 11.6022 10.5 12 10.5C12.3978 10.5 12.7794 10.658 13.0607 10.9393C13.342 11.2206 13.5 11.6022 13.5 12C13.5 12.3978 13.342 12.7794 13.0607 13.0607C12.7794 13.342 12.3978 13.5 12 13.5C11.6022 13.5 11.2206 13.342 10.9393 13.0607C10.658 12.7794 10.5 12.3978 10.5 12ZM10.5 18C10.5 17.6022 10.658 17.2206 10.9393 16.9393C11.2206 16.658 11.6022 16.5 12 16.5C12.3978 16.5 12.7794 16.658 13.0607 16.9393C13.342 17.2206 13.5 17.6022 13.5 18C13.5 18.3978 13.342 18.7794 13.0607 19.0607C12.7794 19.342 12.3978 19.5 12 19.5C11.6022 19.5 11.2206 19.342 10.9393 19.0607C10.658 18.7794 10.5 18.3978 10.5 18Z" fill="#555659"/>
-                                </svg>
-                                </span>
-                            </div>
-                            <div class="task-descriptions">
-                                ${boardsTaskStorage[currentIndex].task_log[ins].description}
-                            </div>
-                            <div class="subtask-container">
-                                <span><h4>Subtask ( 0 of ${boardsTaskStorage[currentIndex].task_log[ins].subtask.length})</h4></span>
-                                <label for="isCompletedSubtaskOne">
-                                <input type="checkbox" name="isCompletedSubtaskOne" id="isCompletedSubtaskOne">
-                                ${dataList}
-                                </label>
-                            </div>
-                            <div class="currentStatus">
-                                <label for="updateStatus">
-                                <span><h4>Status</h4></span>
-                                <select name="updateStatus" id="updateStatus">
-                                    <option value="pending">Pending</option>
-                                    <option value="inprogress">In Progress</option>
-                                    <option value="completed">Completed</option>
-                                </select>
-                                </label>
-                            </div>
-                            <button id="btnModalTaskDetails">
-                                Save
-                            </button>
-                            </div>`;
-
-                        document.body.appendChild(taskDetailModal);
-
-                        var btnModalTaskDetails = taskDetailModal.querySelectorAll('#btnModalTaskDetails');
-                        btnModalTaskDetails.forEach((btnModalTaskDetail, indextask) => {
-                            btnModalTaskDetail.addEventListener('click', () => {
-                            var updateStatus = document.querySelectorAll('#updateStatus');
-                            updateStatus.forEach(itemStatus => {
-                                item.status = itemStatus.value;
-                                localStorage.setItem(storage_key, JSON.stringify(boardsTaskStorage));
-
-                                taskDetailModal.classList.remove('task-detail-modal');
+                        (function(ins) {
+                            sectionItem.children[ins].addEventListener('click', (event) => {
+                                var clickedTask = event.currentTarget;
+                                console.log(clickedTask)
+                                var dataIndex = Array.from(sectionItem.children).indexOf(clickedTask);
+                                var dataList = boardsTaskStorage[currentIndex].task_log[dataIndex].subtask.map(itemList => itemList.statusList);
+                        var taskDetailModal = document.createElement('div');
+                            taskDetailModal.classList.add('task-detail-modal');
+                            
+                            taskDetailModal.innerHTML = '';
+                            
+                            taskDetailModal.innerHTML += `<!-- TASK CONTENT -->
+                                <div class="task-detail-modal-contents">
+                                <div class="task-detail-title">
+                                    <span>
+                                    <h1>${item.task_name}</h1>
+                                    </span>
+                                    <span>
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.5 6C10.5 5.60218 10.658 5.22064 10.9393 4.93934C11.2206 4.65804 11.6022 4.5 12 4.5C12.3978 4.5 12.7794 4.65804 13.0607 4.93934C13.342 5.22064 13.5 5.60218 13.5 6C13.5 6.39782 13.342 6.77936 13.0607 7.06066C12.7794 7.34196 12.3978 7.5 12 7.5C11.6022 7.5 11.2206 7.34196 10.9393 7.06066C10.658 6.77936 10.5 6.39782 10.5 6ZM10.5 12C10.5 11.6022 10.658 11.2206 10.9393 10.9393C11.2206 10.658 11.6022 10.5 12 10.5C12.3978 10.5 12.7794 10.658 13.0607 10.9393C13.342 11.2206 13.5 11.6022 13.5 12C13.5 12.3978 13.342 12.7794 13.0607 13.0607C12.7794 13.342 12.3978 13.5 12 13.5C11.6022 13.5 11.2206 13.342 10.9393 13.0607C10.658 12.7794 10.5 12.3978 10.5 12ZM10.5 18C10.5 17.6022 10.658 17.2206 10.9393 16.9393C11.2206 16.658 11.6022 16.5 12 16.5C12.3978 16.5 12.7794 16.658 13.0607 16.9393C13.342 17.2206 13.5 17.6022 13.5 18C13.5 18.3978 13.342 18.7794 13.0607 19.0607C12.7794 19.342 12.3978 19.5 12 19.5C11.6022 19.5 11.2206 19.342 10.9393 19.0607C10.658 18.7794 10.5 18.3978 10.5 18Z" fill="#555659"/>
+                                    </svg>
+                                    </span>
+                                </div>
+                                <div class="task-descriptions">
+                                    ${boardsTaskStorage[currentIndex].task_log.description}
+                                </div>
+                                <div class="subtask-container">
+                                    <span><h4>Subtask ( 0 of ${item.subtask.length})</h4></span>
+                                    <label for="isCompletedSubtaskOne">
+                                    <input type="checkbox" name="isCompletedSubtaskOne" id="isCompletedSubtaskOne">
+                                    ${dataList}
+                                    </label>
+                                </div>
+                                <div class="currentStatus">
+                                    <label for="updateStatus">
+                                    <span><h4>Status</h4></span>
+                                    <select name="updateStatus" id="updateStatus">
+                                        <option value="pending">Pending</option>
+                                        <option value="inprogress">In Progress</option>
+                                        <option value="completed">Completed</option>
+                                    </select>
+                                    </label>
+                                </div>
+                                <button id="btnModalTaskDetails">
+                                    Save
+                                </button>
+                                </div>`;
+    
+                            document.body.appendChild(taskDetailModal);
+    
+                            var btnModalTaskDetails = taskDetailModal.querySelectorAll('#btnModalTaskDetails');
+                            btnModalTaskDetails.forEach((btnModalTaskDetail, indextask) => {
+                                btnModalTaskDetail.addEventListener('click', () => {
+                                var updateStatus = document.querySelectorAll('#updateStatus');
+                                updateStatus.forEach(itemStatus => {
+                                    item.status = itemStatus.value;
+                                    localStorage.setItem(storage_key, JSON.stringify(boardsTaskStorage));
+    
+                                    taskDetailModal.classList.remove('task-detail-modal');
+                                })
+                                })
+                            })
+    
+                            window.addEventListener('click', (e) => {
+                                if (e.target === taskDetailModal) {
+                                    taskDetailModal.classList.remove('task-detail-modal');
+                                }
                             })
                             })
-                        })
-
-                        window.addEventListener('click', (e) => {
-                            if (e.target === taskDetailModal) {
-                                taskDetailModal.classList.remove('task-detail-modal');
-                            }
-                        })
-                        })
+                        })(ins);
                     }
                     // sectionItem.addEventListener('click', () => {
                         

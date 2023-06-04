446b243c8dd537680ef7c2f50bcba2dead8b1330
Benjie silvano <103920174+kratosman@users.noreply.github.com>
2023-06-04 12:50:47 +0800

    Update tipsScript.js

--- dist/js/tipsScript.js
+++ dist/js/tipsScript.js
@@ -5,6 +5,10 @@ var tipsTime = {
     afternoon_night: 'Wind down your day with a soothing cup of coffee.'
 }
 
+var remindersTime;
+var counter = 0;
+var maxSpeakCount = 2;
+
 function createReminderFunc(time, message) {
     var reminders = document.createElement('div');
     
@@ -18,7 +22,11 @@ function createReminderFunc(time, message) {
 
     var speech = new SpeechSynthesisUtterance(message);
     speechSynthesis.speak(speech);
+    counter++;
 
+    if (counter >= maxSpeakCount) {
+        clearInterval(remindersTime);
+    }
 }
 
 function checkReminders() {
@@ -37,15 +45,17 @@ function checkReminders() {
 
     if (dateHours === 8 && dateMinutes === 30 && period === 'AM') {
         createReminderFunc('8:30 AM', morning_coffee);
-    } else if (dateHours === 12 && dateMinutes === 42 && period === 'PM') {
+    } else if (dateHours === 12 && dateMinutes === 50 && period === 'PM') {
         createReminderFunc('12:00 PM', noon_coffee);
     } else if (dateHours === 3 && dateMinutes === 00 && period === 'PM') {
         createReminderFunc('3:00 AM', afternoon);
     } else if (dateHours === 6 && dateMinutes === 00 && period === 'PM'){
         createReminderFunc('3:00 AM', afternoon_night);
+    } else if (dateMinutes >= 50) {
+        clearInterval(remindersTime);
     }
 
 }
 checkReminders();
 
-var remindersTime = setInterval(checkReminders, 1000);
+remindersTime = setInterval(checkReminders, 1000);
